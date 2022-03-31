a54e127d786b7d27840bf9ed893518d8a78c094a
Martín <magadiflo@gmail.com>
2022-03-31 12:34:04 -0500

    Configuración del gráfico inicial

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/grafico-barra-horizontal.component';
 
@@ -17,6 +20,8 @@ import { GraficoBarraHorizontalComponent } from './grafico-barra-horizontal/graf
   imports: [
     CommonModule,
     RouterModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
   ]
 })
 export class ComponentsModule { }
--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent implements OnInit {
+export class GraficoBarraHorizontalComponent {
+
+  results: any[] = [
+    {
+      "name": "Juego 1",
+      "value": 20
+    },
+    {
+      "name": "Juego 2",
+      "value": 25
+    },
+    {
+      "name": "Juego 3",
+      "value": 15
+    },
+    {
+      "name": "Juego 4",
+      "value": 30
+    }
+  ];
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Juegos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votos';
+
+  colorScheme = 'nightLights';
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSelect(event: any) {
+    console.log(event);
   }
+
+
 
 }
